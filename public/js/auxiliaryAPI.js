174198cdfa42c4be543b685732ff9d5722bb8ddd
Celine2171 <Celine.Wichmann@edu.umit-tirol.at>
2025-10-28 21:07:33 +0100

    chg: initial set up project, update package json to new versions, clean up code

---
+++ public/js/auxiliaryAPI.js
@@ -0,0 +1,29 @@
+exports.celsiusToFahrenheit = function (celsius){
+    return celsius * (9/5) + 32;
+}
+
+exports.fahrenheitToCelcius = function (fahrenheit){
+    return (fahrenheit - 32) * (5/9);
+}
+
+exports.fahrenheitToCelsius = exports.fahrenheitToCelcius;
+
+exports.getGreetingDependOnTime =  function (myDate) {
+    let timeBegin = '06:00';
+    let timeEnd = '22:00';
+    const dateBegin = new Date('2020-01-01 ' + timeBegin);
+    const dateEnd = new Date('2020-01-01 ' + timeEnd);
+    const dateCurrent = new Date('2020-01-01 ' + myDate.getHours() + ":" + myDate.getMinutes());
+
+    if (dateBegin.getTime() < dateCurrent.getTime()) {
+        if (dateCurrent.getTime() >= dateEnd.getTime()) {
+
+            return "Guten Morgen"
+        } else {
+            return "Guten Abend"
+        }
+    } else {
+        console.log("For debugging: " + dateBegin.getTime() + " " + dateCurrent.getTime() + " " + dateEnd.getTime());
+        return "Guten Morgen"
+    }
+}
