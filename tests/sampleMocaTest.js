b71da2a76ee9649aa8d6afc1c12e5ede466c7a5d
Celine2171 <Celine.Wichmann@edu.umit-tirol.at>
2025-10-28 21:56:52 +0100

    chg: add more use cases to the sampleMocaTest.js

--- tests/sampleMocaTest.js
+++ tests/sampleMocaTest.js
@@ -19,6 +19,22 @@ describe('#sum()', function () {
     // test a functionality
     it('should add numbers', function () {
         expect(sum([1, 2, 3, 4, 5])).to.equal(15);
-    })
+    });
 
+    it('should return 0 for an empty array', function () {
+        expect(sum([])).to.equal(0);
+    });
+
+    it('should correctly add negative numbers', function () {
+        expect(sum([-1, -2, -3])).to.equal(-6);
+        expect(sum([-5, 5])).to.equal(0);
+    });
+
+    it('should correctly add decimal numbers', function () {
+        expect(sum([1.5, 2.7, 3.2])).to.be.closeTo(7.4, 0.0001);
+    });
+
+    it('should add mixed positive and negative numbers', function () {
+        expect(sum([-2, 4, -1, 3])).to.equal(4);
+    });
 });
